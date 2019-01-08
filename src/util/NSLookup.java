133e6d37111666b9784caa4cf862baf41c08e219
박성혜 <gps03155@gmail.com>
2019-01-08 15:52:01 +0900

    HomeWork - NSLookup add

--- src/util/NSLookup.java
+++ src/util/NSLookup.java
@@ -2,13 +2,28 @@ package util;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class NSLookup {
 
 	public static void main(String[] args) {
-		String line = "www.naver.com";
 		try {
-			InetAddress[] inetAddress = InetAddress.getAllByName(line);
+			Scanner sc = new Scanner(System.in);
+
+			while (true) {
+				System.out.print("> ");
+				String input = sc.nextLine();
+				
+				if(input.equals("exit")) {
+					break;
+				}
+				InetAddress[] inetAddress = InetAddress.getAllByName(input);
+				
+				for(int i=0;i<inetAddress.length;i++) {
+					System.out.println(input + " : " + inetAddress[i].getHostAddress());
+				}
+			}
+
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
